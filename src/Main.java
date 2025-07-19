185096ff5b0b7264ed9943a1dfb36a304dc1d10f
Paula Galdames <sitio.galdames@gmail.com>
2025-07-18 22:04:58 -0400

    Conexión a la base de datos exitosa

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            //establecer conexion
+            String url = "jdbc:mysql://localhost:3306/bibliojuegos";
+            String user = "root";
+            String password = "";
+
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexión a la base de datos exitosa.");
+
+            //aca viene el resto del codigo
+
+
+        }catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            try{
+                if(resultSet != null) resultSet.close();
+                if(preparedStatement != null) preparedStatement.close();
+                if(connection != null) connection.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
     }
 }
